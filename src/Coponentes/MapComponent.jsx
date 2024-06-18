99ab8d685f89d7627a9e62f7e18db146722226d0
josueDanielpp <parradavalosj@gmail.com>
2024-06-17 20:34:51 -0600

    tomar de endpoint

--- src/Coponentes/MapComponent.jsx
+++ src/Coponentes/MapComponent.jsx
@@ -1,8 +1,9 @@
 // src/MapComponent.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+import axios from 'axios';
 
 // Ajuste para los íconos de Leaflet con React
 import iconUrl from 'leaflet/dist/images/marker-icon.png';
@@ -17,24 +18,51 @@ let DefaultIcon = L.icon({
 L.Marker.prototype.options.icon = DefaultIcon;
 
 const MapComponent = () => {
-    const position = [51.505, -0.09]; // Coordenadas de ejemplo
+    const [positions, setPositions] = useState([
+        { latitud: 51.505, longitud: -0.09 },
+        { latitud: 51.51, longitud: -0.1 },
+        { latitud: 51.51, longitud: -0.08 }
+    ]);
+
+    useEffect(() => {
+        const fetchPositions = async () => {
+            try {
+                const response = await axios.get('https://api-proxy-ie.onrender.com/location/get_all');
+                console.log('response:', response.data);
+                if (response.data?.length == 0 || response.data?.length == undefined || response.data?.length == null || !response.data) {
+                    return console.log('No data found');
+                } else {
+                    setPositions(response.data);
+                }
+
+            } catch (error) {
+                console.error('Error fetching positions:', error);
+            }
+        };
+
+        fetchPositions();
+    }, []);
 
     return (
-        <div style={{width:"100%"}}>
-            <h1>Ubicacion espacioal con GPS en tiempo real</h1>
-            <MapContainer center={position} zoom={13} style={{ height: "100vh", width: "100%" }}>
+        <div style={{ width: "100%" }}>
+            <div style={{display:"flex", justifyContent:"center"}}>
+                <h1>UBICACIÓN EN EL MAPA</h1>
+            </div>
+
+            <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: "100vh", width: "100%" }}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
-                <Marker position={position}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                {positions?.map((position, index) => (
+                    <Marker key={index} position={[position.latitud, position.longitud]}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                ))}
             </MapContainer>
         </div>
-
     );
 };
 
